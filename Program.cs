a4563e751410851ec72dcf369bc3caf812c1fec5
cal <m7od99@icloud.com>
2020-11-28 22:39:17 +0300

    adding Multiplication class and the md file for it

--- Program.cs
+++ Program.cs
@@ -11,7 +11,13 @@ namespace QuestionsAndAnswerss
            Distance.Edit_Distance("love you" ,"ve k bal");
 
            Distance.Edit_Distance("lk", "love");
-           
+
+           Console.WriteLine(Multiplication.DoMultiplication("3", "2")); // 6
+           Console.WriteLine(Multiplication.DoMultiplication("100", "5")); // 500
+           Console.WriteLine(Multiplication.DoMultiplication("6", "6")); // 36
+           Console.WriteLine(Multiplication.DoMultiplication("5", "5")); // 25
+
+  
 
         }
     }
---
+++ Q11-Multip.md
@@ -0,0 +1,24 @@
+program that does only multiplication, but without utilising the multiplication operator or any library . 
+
+
+
+
+
+input = two string . 
+
+output = the integer after calcltion .
+
+
+
+
+
+-0 initialize Answer and set it to 0 .
+
+
+-at first we need to make sure the input is number .
+
+-then convert the strings to intgers by using ASCII Table .
+
+-Repeat the addition of the first number by itself as much as the second number and save it in Answer 
+
+-at end return answer .
