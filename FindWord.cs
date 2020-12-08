b56f3f8878d59a52097a7d07f5597e0ab3b6bb82
cal <m7od99@icloud.com>
2020-12-08 21:31:13 +0300

    add Find word class with stpes

---
+++ FindWord.cs
@@ -0,0 +1,16 @@
+namespace QuestionsAndAnswerss
+{
+    public class FindWord
+    {
+
+        //  -0 initialize index and set it 0 .
+
+        // -0 initialize isFind set it as false .
+
+        // -0 method to cheak if still character in string or not by minus index from length .
+
+        // -0 method that cheak if all character of word in the string 
+
+        // -0 if yes return true and return index of first char else return -1
+    }
+}
